0ced3a4414572162bee33f4bdc4baf9ceae3207e
AnzilottiChristopher <anzilottichr@gmail.com>
2023-02-23 18:01:59 -0500

    First Commit

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -2,6 +2,6 @@ public class AnimalPicker
 {
     public static void main(String[] args) throws Exception 
     {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); //Making a change to commit
     }
 }
